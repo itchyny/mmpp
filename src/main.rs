dc9126d4d8ef9c33f7f7c9a1c3065884c6c98783
itchyny <itchyny@hatena.ne.jp>
2017-09-23 21:17:17 +0900

    implement mmpp cli tool

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::io::{self, Read};
+
+extern crate mmpp;
+
+fn main() {
+    let mut buffer = String::new();
+    let _ = io::stdin().read_to_string(&mut buffer);
+    match mmpp::parse_metric(buffer.as_ref()) {
+        Ok(metric) => println!("{}", mmpp::pretty_print(metric)),
+        Err(err) => println!("{}", err),
+    }
+}
