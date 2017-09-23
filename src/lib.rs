bb0a8bc7c48f57e6ec98d26e1087035c485f15fe
itchyny <itchyny@hatena.ne.jp>
2017-09-23 11:11:56 +0900

    support role slot metric

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ pub enum Graph {
     HostMetric(String, String),
     ServiceMetric(String, String),
     RoleMetric(String, String, String),
+    RoleSlotMetric(String, String, String),
 }
 
 pub fn parse_graph(src: &str) -> Result<Graph, String> {
@@ -39,6 +40,14 @@ pub fn parse_graph(src: &str) -> Result<Graph, String> {
             let metric_name = inner.next().unwrap().into_inner().next().unwrap().as_str().to_string();
             Ok(Graph::RoleMetric(service_name, role_name, metric_name))
         }
+        Rule::role_slot_metric => {
+            let mut inner = pair.into_inner();
+            let mut role_full_name = inner.next().unwrap().into_inner().next().unwrap().into_inner();
+            let service_name = role_full_name.next().unwrap().as_str().to_string();
+            let role_name = role_full_name.next().unwrap().as_str().to_string();
+            let metric_name = inner.next().unwrap().into_inner().next().unwrap().as_str().to_string();
+            Ok(Graph::RoleSlotMetric(service_name, role_name, metric_name))
+        }
         _ => unreachable!(),
     }
 }
@@ -63,7 +72,9 @@ mod tests {
                  ("role(Blog:db, memory.*)",
                   Graph::RoleMetric("Blog".to_string(), "db".to_string(), "memory.*".to_string())),
                  ("role (  'Blog:  db' , 'memory.*'  ) ",
-                  Graph::RoleMetric("Blog".to_string(), "db".to_string(), "memory.*".to_string()))];
+                  Graph::RoleMetric("Blog".to_string(), "db".to_string(), "memory.*".to_string())),
+                 ("roleSlots (  Blog:db , loadavg5  ) ",
+                  Graph::RoleSlotMetric("Blog".to_string(), "db".to_string(), "loadavg5".to_string()))];
         for (source, expected) in sources {
             let got = parse_graph(source);
             assert_eq!(got, Ok(expected));
